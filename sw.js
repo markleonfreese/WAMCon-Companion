e2e72d1d3328ac595efceda91ce9efc42b0a23eb
markleonfreese <46199304+markleonfreese@users.noreply.github.com>
2026-08-14 14:10:06 +0800

    fix: refresh offline cache for enriched program

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="wamcon-companion-v1";
+const CACHE="wamcon-companion-v2";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./schedule.json","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
