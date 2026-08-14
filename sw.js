cb640795996ad07e660d230a2ba9f166f3321b69
markleonfreese <46199304+markleonfreese@users.noreply.github.com>
2026-08-14 14:30:27 +0800

    fix: activate PWA updates immediately

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
-const CACHE="wamcon-companion-v3";
+const CACHE="wamcon-companion-v4";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./import.js","./schedule.json","./manifest.json","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("install",e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener("activate",e=>{
+  e.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+    self.clients.claim()
+  ]));
+});
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
