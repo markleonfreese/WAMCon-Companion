fdb3ad44e142fa05f8ca6921157a93682c6bb04f
markleonfreese <46199304+markleonfreese@users.noreply.github.com>
2026-08-15 19:10:19 +0800

    chore: refresh PWA cache for artist profiles

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="wamcon-companion-v6";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./import.js","./schedule.json","./showcase.json","./manifest.json","./icon-192.png","./icon-512.png"];
+const CACHE="wamcon-companion-v7";
+const ASSETS=["./","./index.html","./styles.css","./app.js","./profiles.js","./import.js","./artists.json","./schedule.json","./showcase.json","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
