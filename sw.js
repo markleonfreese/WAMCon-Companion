c9b6922f02764b1a615813db9c08e0e0de4654aa
markleonfreese <46199304+markleonfreese@users.noreply.github.com>
2026-08-15 19:06:51 +0800

    chore: cache live Showcase timetable

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="wamcon-companion-v5";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./import.js","./schedule.json","./manifest.json","./icon-192.png","./icon-512.png"];
+const CACHE="wamcon-companion-v6";
+const ASSETS=["./","./index.html","./styles.css","./app.js","./import.js","./schedule.json","./showcase.json","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
